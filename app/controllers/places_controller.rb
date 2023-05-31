d65c3dc462b33ecfc6c3230b42b96afe7861f568
Carlo Fava <carlo.fava@kellogg.northwestern.edu>
2023-05-31 19:40:37 +0000

    v1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,28 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.all.where({ user_id: session[:user_id] })
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    @posts = Post.where({ "place_id" => @place["id"], "user_id" => session["user_id"]})
   end
 
   def new
     @place = Place.new
+    @place.user_id = params["user_id"]
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["place"]["name"]
-    @place.save
-    redirect_to "/places"
+    if @current_user
+      @place = Place.new(params["place"])
+      @place.user_id = @current_user.id
+      @place.save
+    else
+      flash[:notice] = "Please Login."
+    end
+      redirect_to "/places"
   end
 
 end
